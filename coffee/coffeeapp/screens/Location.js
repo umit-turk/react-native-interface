3765eac788a12979d4517dd78d36675dc62408c3
umitturk <uyt_turk@hotmail.com>
2021-10-25 16:50:59 +0300

    locations screen

--- coffee/coffeeapp/screens/Location.js
+++ coffee/coffeeapp/screens/Location.js
@@ -1,31 +1,292 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
-    View,
-    Text,
-    TouchableOpacity,
-    StyleSheet
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  TextInput,
+  FlatList,
 } from 'react-native';
+import {dummyData, COLORS, SIZES, icons, FONTS} from '../constants';
+import {connect} from 'react-redux';
+import {IconButton, TabButton} from '../components';
 
-const Location = ({ navigation }) => {
+const Location = ({navigation, appTheme}) => {
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  function renderHeader() {
     return (
-        <View style={styles.container}>
-            <Text>Location</Text>
+      <SafeAreaView
+        style={{
+          height: 120,
+          backgroundColor: COLORS.primary,
+          alignItems: 'center',
+        }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            paddingHorizontal: SIZES.radius,
+          }}>
+          {/* Back Button */}
+          <IconButton
+            icon={icons.leftArrow}
+            onPress={() => navigation.goBack()}
+          />
+          {/* Title */}
+          <View
+            style={{
+              flex: 1,
+              alignItems: 'center',
+            }}>
+            <Text style={{color: COLORS.white, ...FONTS.h1, fontSize: 25}}>
+              Locations
+            </Text>
+          </View>
 
-            <TouchableOpacity
-                onPress={() => navigation.navigate("Order")}
-            >
-                <Text>Navigate to Order</Text>
-            </TouchableOpacity>
+          {/* Empty */}
+          <View style={{width: 25}}></View>
         </View>
-    )
-}
+      </SafeAreaView>
+    );
+  }
+
+  function renderTopBarSection() {
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+        }}>
+        {/* Nearby  */}
+        <TabButton
+          containerStyle={{
+            width: 80,
+          }}
+          label="Nearby"
+          selected={selectedTab == 0}
+          onPress={() => setSelectedTab(0)}
+        />
+
+        {/* Previous */}
+        <TabButton
+          containerStyle={{
+            width: 100,
+          }}
+          label="Previous"
+          selected={selectedTab == 1}
+          onPress={() => setSelectedTab(1)}
+        />
+        {/* Favouriti */}
+        <TabButton
+          containerStyle={{
+            width: 100,
+          }}
+          label="Favorite"
+          selected={selectedTab == 2}
+          onPress={() => setSelectedTab(2)}
+        />
+      </View>
+    );
+  }
+
+  function renderSearchBar() {
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          marginTop: SIZES.radius,
+          height: 50,
+          paddingHorizontal: SIZES.padding,
+          borderRadius: 25,
+          backgroundColor: COLORS.lightGreen2,
+          alignItems: 'center',
+        }}>
+        <TextInput
+          style={{
+            flex: 1,
+            height: 50,
+            color: COLORS.black,
+            ...FONTS.body3,
+          }}
+          placeholder="Enter your city, state or zip code"
+          placeholderTextColor={COLORS.lightGray2}
+        />
+        <Image
+          source={icons.search}
+          style={{
+            width: 30,
+            height: 30,
+            tintColor: COLORS.lightGray2,
+          }}
+        />
+      </View>
+    );
+  }
+
+  function renderLocationList() {
+    return (
+      <FlatList
+        style={{
+          marginTop: SIZES.radius,
+          paddingHorizontal: SIZES.radius,
+        }}
+        data={dummyData.locations}
+        keyExtractor={item => item.id}
+        showsVerticalScrollIndicator={false}
+        keyboardDismissMode="on-drag"
+        renderItem={({item}) => (
+          <TouchableOpacity
+            style={{
+              marginBottom: SIZES.radius,
+              borderRadius: SIZES.radius * 2,
+              paddingHorizontal: SIZES.padding,
+              paddingVertical: SIZES.radius,
+              backgroundColor: appTheme.cardBackgroundColor,
+            }}
+            onPress={() =>
+              navigation.navigate('Order', {selectedLocation: item})
+            }>
+            {/* Name and Bookark */}
+            <View
+              style={{
+                flexDirection: 'row',
+              }}>
+              <Text style={{flex: 1, color: appTheme.textColor, ...FONTS.h2}}>
+                {item.title}
+              </Text>
+
+              <Image 
+              source={item.bookmarked ? icons.bookmarkFilled : icons.bookmark}
+              style={{
+                  height: 20,
+                  width:20,
+                  tintColor:item.bookmarked ? COLORS.red2 : COLORS.white
+              }}
+              />
+            </View>
+
+            {/* Address */}
+
+            <View
+            style={{
+                marginTop:SIZES.base,
+                width:"80%"
+            }}
+            >
+                <Text
+                style={{
+                    color:appTheme.textColor,
+                    ...FONTS.body3,
+                    lineHeight:21
+                }}
+                >
+                    {item.address}
+                </Text>
+            </View>
+
+            {/* Operation Hours */}
+            <View
+            style={{
+                marginTop:SIZES.base
+            }}
+            >
+                <Text
+                style={{
+                    color:appTheme.textColor,
+                    ...FONTS.body5,
+                    lineHeight:16
+                }}
+                >{item.operation_hours}
+                </Text>
+            </View>
+
+            {/* Services */}
+                <View
+                style={{
+                    flexDirection:"row",
+                    marginTop:SIZES.base
+                }}
+                >
+                    {/* Pick up */}
+                    <View
+                    style={{
+                        borderColor:appTheme.textColor,
+                        borderWidth:1,
+                        borderRadius:20,
+                        paddingHorizontal:SIZES.radius,
+                        paddingVertical:5
+                    }}
+                    >
+                        <Text style={{
+                            color:appTheme.textColor,
+                            ...FONTS.body3
+                        }}>Pick-Up</Text>
+                    </View>
+
+                    {/* Delivery */}
+                    <View
+                    style={{
+                        borderColor:appTheme.textColor,
+                        borderWidth:1,
+                        borderRadius:20,
+                        paddingHorizontal:SIZES.radius,
+                        paddingVertical:5,
+                        marginLeft:5
+                    }}
+                    >
+                        <Text style={{
+                            color:appTheme.textColor,
+                            ...FONTS.body3
+                        }}>Delivery</Text>
+                    </View>
+                </View>
+
+          </TouchableOpacity>
+        )}
+      />
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      {/* Header */}
+      {renderHeader()}
+
+      {/* Detail */}
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: appTheme.backgroundColor,
+          marginTop: -20,
+          borderTopLeftRadius: SIZES.radius * 2,
+          borderTopRightRadius: SIZES.radius * 2,
+          padding: SIZES.padding,
+        }}>
+        {renderTopBarSection()}
+        {renderSearchBar()}
+        {renderLocationList()}
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
+  container: {
+    flex: 1,
+  },
+});
 
-export default Location;
+function mapStateToProps(state) {
+  return {
+    appTheme: state.appTheme,
+    error: state.error,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Location);
--- coffee/coffeeapp/screens/Rewards.js
+++ coffee/coffeeapp/screens/Rewards.js
@@ -1,24 +1,200 @@
 import React from 'react';
-import {
-    View,
-    Text,
-    StyleSheet
-} from 'react-native';
+import {View, Text, StyleSheet, FlatList, ImageBackground} from 'react-native';
+import {HeaderBar, CustomButton} from '../components';
+import {dummyData, COLORS, SIZES, FONTS, icons} from '../constants';
+import {connect} from 'react-redux';
 
-const Rewards = () => {
+const Rewards = ({navigation, appTheme}) => {
+  function renderRewardPointSection() {
     return (
-        <View style={styles.container}>
-            <Text>Rewards</Text>
-        </View>
-    )
-}
+      <View
+        style={{
+          alignItems: 'center',
+          marginVertical: SIZES.padding,
+        }}>
+        {/* Text */}
+        <Text
+          style={{
+            color: COLORS.primary,
+            ...FONTS.h1,
+            fontSize: 35,
+          }}>
+          Rewards
+        </Text>
+        <Text
+          style={{
+            marginTop: 10,
+            color: appTheme.textColor,
+            width: SIZES.width * 0.6,
+            textAlign: 'center',
+            ...FONTS.h3,
+            lineHeight: 18,
+          }}>
+          You are 60 points away from your next reward
+        </Text>
+
+        {/* Image */}
+        <ImageBackground
+          source={icons.reward_cup}
+          resizeMode="contain"
+          style={{
+            marginTop: SIZES.padding,
+            width: SIZES.width * 0.8,
+            height: SIZES.width * 0.8,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <View
+            style={{
+              width: 70,
+              height: 70,
+              borderRadius: 35,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: COLORS.white,
+            }}>
+            <Text style={{...FONTS.h1}}>280</Text>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
+  function renderButtons(){
+      return (
+          <View
+          style={{
+              flexDirection:"row",
+              alignItems:"center",
+              justifyContent:"center"
+          }}
+          >
+              {/* Scan Button */}
+              <CustomButton 
+              isPrimaryButton={true}
+              label="Scan in store"
+              containerStyle={{
+                  width:130,
+                  paddingVertical:5,
+                  marginRight: SIZES.radius,
+                  borderRadius: SIZES.radius * 2,
+              }}
+              labelStyle={{
+                  ...FONTS.h3
+              }}
+              onPress={() => navigation.navigate('Location')}
+              />
+
+              {/* Redeem Button */}
+              <CustomButton 
+              isSecondaryButton={true}
+              label="Redeem"
+              containerStyle={{
+                  width:130,
+                  paddingVertical:5,
+                  borderRadius:SIZES.radius * 2
+              }}
+              labelStyle={{
+                  ...FONTS.h3
+              }}
+              onPress={() => navigation.navigate('Location')}
+              />
+
+          </View>
+      )
+  }
+  function renderAvailableRewardsHeader(){
+      return (
+          <View
+          style={{
+              marginTop:SIZES.padding,
+              marginBottom:SIZES.radius,
+              paddingHorizontal:SIZES.padding
+          }}
+          >
+              <Text style={{color:appTheme.textColor, ...FONTS.h2}}>Available Rewards</Text>
+          </View>
+      )
+  }
+
+  return (
+    <View style={styles.container}>
+      {/* Header */}
+      <HeaderBar />
+
+      {/* Details */}
+      <FlatList
+        style={{
+          marginTop: -25,
+          borderTopLeftRadius: SIZES.radius * 2,
+          borderTopRightRadius: SIZES.radius * 2,
+          backgroundColor: appTheme.backgroundColor,
+        }}
+        data={dummyData.availableRewards}
+        keyExtractor={item => `${item.id}`}
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={
+          <View>
+            {/* Reward Point */}
+            {renderRewardPointSection()}
+
+            {/* Buttons */}
+            {renderButtons()}
+
+            {/* Header Label */}
+            {renderAvailableRewardsHeader()}
+          </View>
+        }
+        renderItem={({item}) => {
+          return (
+            <View
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                marginHorizontal: SIZES.padding,
+                marginBottom: SIZES.base,
+                paddingVertical: SIZES.base,
+                borderRadius: 20,
+                backgroundColor: item.eligible ? COLORS.yellow : COLORS.gray2,
+              }}>
+              <Text
+                style={{
+                  color: item.eligible ? COLORS.black : COLORS.lightGray2,
+                  ...FONTS.body3,
+                }}>
+                {item.title}
+              </Text>
+            </View>
+          );
+        }}
+        ListFooterComponent={
+            <View
+            style={{
+                marginBottom:120
+            }}
+            >
+
+            </View>
+        }
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
+  container: {
+    flex: 1,
+  },
+});
 
-export default Rewards;
+function mapStateToProps(state) {
+  return {
+    appTheme: state.appTheme,
+    error: state.error,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Rewards);
